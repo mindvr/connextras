a179ed47d1b0d6548acbba9c47ebe3fcd65d3822
Stepan Kuznetsov <mindvr@gmail.com>
2025-01-04 20:28:56 +0500

    remove ng init placeholders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DragDropTilesComponent} from '../drag-drop-tiles/drag-drop-tiles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DragDropTilesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'connextras';
 }
