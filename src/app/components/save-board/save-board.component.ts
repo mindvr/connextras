30137657647faec36f9e82821aba4cb59abf07d4
Stepan Kuznetsov <mindvr@gmail.com>
2025-01-06 21:38:51 +0500

    extend row model to include color

--- src/app/components/save-board/save-board.component.ts
+++ src/app/components/save-board/save-board.component.ts
@@ -1,39 +1,37 @@
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {ConnectionsBoardService} from '../../service/connections-board.service';
-import {Row} from '../../service/connections-board.service';
+import {ConnectionsBoardService, Group, Row} from '../../service/connections-board.service';
 
 @Component({
-  selector: 'app-save-board',
-  templateUrl: './save-board.component.html',
-  styleUrls: ['./save-board.component.css'],
-  imports: [FormsModule]
+    selector: 'app-save-board',
+    templateUrl: './save-board.component.html',
+    styleUrls: ['./save-board.component.css'],
+    imports: [FormsModule]
 })
 export class SaveBoardComponent {
-  inputValue: string = '';
+    inputValue: string = '';
 
-  constructor(private connectionsBoard: ConnectionsBoardService) {
-  }
+    constructor(private connectionsBoard: ConnectionsBoardService) {
+    }
 
-  updateValue(): void {
-    // split input value by commas, trim whitespace, remove empty strings, uppercase and store in array
-    let board = this.inputValue.split(',')
-      .map((item) => item.trim())
-      .filter((item) => item !== '')
-      .map((item) => item.toUpperCase());
-// split into rows of 4 elements, map each to Row object with tiles[] property and add one empty row at the end
-    let rows: Row[] = board.reduce((acc: Row[], item, index) => {
-      if (index % 4 === 0) {
-        acc.push({id: `Row${acc.length + 1}`, tiles: []});
-      }
-      acc[acc.length - 1].tiles.push(item);
-      return acc;
-    }, []);
+    updateValue(): void {
+        // split input value by commas, trim whitespace, remove empty strings, uppercase and store in array
+        let board = this.inputValue.split(',')
+            .map((item) => item.trim())
+            .filter((item) => item !== '')
+            .map((item) => item.toUpperCase());
+        let rows: Row[] = board.reduce((acc: Row[], item, index) => {
+            if (index % 4 === 0) {
+                acc.push({id: `Row${acc.length + 1}`, tiles: [], group: Group.None, comment: ''});
+            }
+            acc[acc.length - 1].tiles.push(item);
+            return acc;
+        }, []);
 
-    // Add one empty row at the end
-    rows.push({id: `Row${rows.length + 1}`, tiles: []});
+        // Add one empty row at the end
+        rows.push({id: `Row${rows.length + 1}`, tiles: [], group: Group.None, comment: ''});
 
-    // Update the rows in the service
-    this.connectionsBoard.setRows(rows);
-  }
+        // Update the rows in the service
+        this.connectionsBoard.setRows(rows);
+    }
 }
